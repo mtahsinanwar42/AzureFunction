090169f6cbeb5ad4a2f4fe135638f39cbb07f9af
mtahsinanwar42 <mtahsinanwar42@gmail.com>
2025-04-14 14:09:13 +0200

    Added Loggers

--- function_app.py
+++ function_app.py
@@ -38,10 +38,10 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello from Yuhang, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "!!!!! Hello Yuhang.T!his HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
 
